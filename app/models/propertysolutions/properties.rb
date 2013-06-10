ad4e9fbbc4910e181896485a3a97d8178ea20201
William Barry <williambarry007@gmail.com>
2013-06-10 18:28:04 -0500

    More changes

--- app/models/propertysolutions/properties.rb
+++ app/models/propertysolutions/properties.rb
@@ -3,38 +3,38 @@ class PropertySolutions::Properties < PropertySolutions::ApiConsumer
   
   @base = 'properties'
   
-  def floor_plans(property_id, property_floor_plan_ids = nil)
+  def self.floor_plans(property_id, property_floor_plan_ids = nil)
     return self.request('getFloorPlans', { 
       'propertyId' => property_id,
       'propertyFloorPlanIds' => property_floor_plan_ids
     })
   end
   
-  def pet_types(property_ids)
+  def self.pet_types(property_ids)
     return self.request('getGetTypes', { 
       'propertyIds' => property_ids 
     })
   end
   
-  def rentable_items(property_id)
+  def self.rentable_items(property_id)
     return self.request('getRentableItems', { 
       'propertyId' => property_id 
     })
   end
   
-  def property_pick_lists(property_ids)
+  def self.property_pick_lists(property_ids)
     return self.request('getPropertyPickLists', { 
       'propertyIds' => property_ids 
     })
   end
   
-  def properties(property_ids = nil)
+  def self.properties(property_ids = nil)
     return self.request('getPropertyPickLists', { 
       'propertyIds' => property_ids 
     })
   end
   
-  def property_preferences(property_id, keys = nil)
+  def self.property_preferences(property_id, keys = nil)
     return self.request('getPropertyPreferences', { 
       'property_id' => property_id
       'keys' => keys
