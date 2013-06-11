fb9a9f1c54f75d11c7ed0c0f63b4f47dee739293
William Barry <williambarry007@gmail.com>
2013-06-11 08:04:18 -0500

    More changes

--- app/models/property_solutions/api_consumer.rb
+++ app/models/property_solutions/api_consumer.rb
@@ -5,14 +5,14 @@ class PropertySolutions::ApiConsumer
 
     data.delete_if { |k,v| v.nil? }    
     data = {
-      "auth": {
-        "type" : "basic",
-        "username": PropertySolutions::username,
-        "password": PropertySolutions::password,
+      "auth" => {
+        "type"      => "basic",
+        "username"  => PropertySolutions::username,
+        "password"  => PropertySolutions::password,
       },
-      "method": {
-        "name": method, # required_service_name
-        "params": data # use the parameters required for the web service here
+      "method" => {
+        "name"    => method,  # required_service_name
+        "params"  => data     # use the parameters required for the web service here
       }
     }
     
